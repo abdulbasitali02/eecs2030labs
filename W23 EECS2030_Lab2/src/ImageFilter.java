98b26603ae8deffc9cfb5f5fb88786e7c5a90b20
abdulbasitali02 <103627220+abdulbasitali02@users.noreply.github.com>
2023-01-29 19:39:56 -0500

    Add files via upload

---
+++ W23 EECS2030_Lab2/src/ImageFilter.java
@@ -0,0 +1,32 @@
+
+/**
+ * @author Andriy Pavlovych
+ * The class is meant to illustrate a couple of image-processing algorithms:
+ * three R-G-B channel extraction methods
+ */
+public class ImageFilter{
+	/**
+	 * private constructor, by design
+	 */
+	private ImageFilter() {		
+	}
+
+	//TODO create proper JavaDoc comments
+	public static int [] getRed(int[] imageData, int width) {
+		//TODO
+		return imageData;
+	}
+
+	//TODO create proper JavaDoc comments
+	public static int [] getGreen(int[] imageData, int width) {
+		//TODO
+		return imageData;
+	}
+
+	//TODO create proper JavaDoc comments
+	public static int [] getBlue(int[] imageData, int width) {
+		//TODO
+		return imageData;
+	}
+
+}
