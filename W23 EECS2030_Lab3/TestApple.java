237c31069c12391c62e288d0baaad6e81ef2a8b6
abdulbasitali02 <103627220+abdulbasitali02@users.noreply.github.com>
2023-02-05 16:32:10 -0500

    ..

---
+++ W23 EECS2030_Lab3/TestApple.java
@@ -0,0 +1,5 @@
+public class TestApple {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ W23 EECS2030_Lab3/mainTest.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+import java.util.Calendar;
+
+public class mainTest {
+  public static void main(String[] args) {
+    Apple mynewApple = new Apple("fiji", 154.8);
+    System.out.println(mynewApple.type);
+  }
+
+    
+
+  //  Apple newApple = new Apple("fiji", 57.8, date);
+    
+}
