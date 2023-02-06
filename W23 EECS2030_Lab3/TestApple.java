47f540813eeb18a3155e5d132b8a9bd5318618dd
abdulbasitali02 <103627220+abdulbasitali02@users.noreply.github.com>
2023-02-05 20:07:27 -0500

    Update TestApple.java

--- W23 EECS2030_Lab3/TestApple.java
+++ W23 EECS2030_Lab3/TestApple.java
@@ -1,5 +1,11 @@
 public class TestApple {
     public static void main(String[] args) {
         
+        String hello = "hello";
+        String world = "world";
+        String helloWorld = hello + world;
+
+        System.out.println(helloWorld);
+
     }
 }
