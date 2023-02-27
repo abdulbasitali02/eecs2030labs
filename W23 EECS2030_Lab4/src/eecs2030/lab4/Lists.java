5481ccf7a8f4bebd71aca20b4f23980f2ec7825a
abdulbasitali02 <103627220+abdulbasitali02@users.noreply.github.com>
2023-02-27 17:32:08 -0500

    ..

--- W23 EECS2030_Lab4/src/eecs2030/lab4/Lists.java
+++ W23 EECS2030_Lab4/src/eecs2030/lab4/Lists.java
@@ -12,24 +12,24 @@ public class Lists {
 	}
 
 	public static <T extends Comparable <? super T>> void insertionSortRecursive(List <T> list){
+
 		//base case
 		if (list.size() <= 1){
 			return;
 		}
-		
-		//Sort the first n-1 elements
-		insertionSortRecursive(list.subList(0, list.size()-1));
-
-		//Insert the nth element into the sorted list
-		T item = list.get(list.size()-1);
-		int j = list.size() - 2;
-		while (j >= 0 && list.get(j).compareTo(item) > 0){
-			list.set(j+1, list.get(j));
-			j--;
+		insertionSortRecursive(list.subList(1, list.size()));
+		T first = list.get(0);
+		T second = list.get(1);
+		if (first.compareTo(second) > 0){
+			list.set(0, second);
+			list.set(1, first);
+			insertionSortRecursive(list.subList(1, list.size()));
 		}
-		list.set(j+1, item);
+		
 	}
 	
+	
+
 	public static <T extends Comparable <? super T>> void insertionSortIterative(List <T> list){
 		int i = 1;
 		
