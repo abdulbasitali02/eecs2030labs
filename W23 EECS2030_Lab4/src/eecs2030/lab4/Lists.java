c1c5a87b98b0dce03c07f514e9718ac5592f1443
abdulbasitali02 <103627220+abdulbasitali02@users.noreply.github.com>
2023-03-06 18:16:24 -0500

    completed the lab

--- W23 EECS2030_Lab4/src/eecs2030/lab4/Lists.java
+++ W23 EECS2030_Lab4/src/eecs2030/lab4/Lists.java
@@ -12,19 +12,29 @@ public class Lists {
 	}
 
 	public static <T extends Comparable <? super T>> void insertionSortRecursive(List <T> list){
-		if (list.size() < 2){
+		// base case: list is empty or has only one element
+		if (list.size() <= 1){
 			return;
 		}
-		insertionSortRecursive(list.subList(1, list.size()));
-		T first = list.get(0);
-		T second = list.get(1);
-		if (second.compareTo(first) > 0){
-			list.set(0, second);
-			list.set(1, first);
-			insertionSortRecursive(list.subList(0, list.size()-1));
-		}		
-	}	
+		// recursive case: list has more than one element
+		// remove the last element from the list
+		T item = list.remove(list.size() - 1);
+		
+		insertionSortRecursive(list);	
+		insertionSortRecursiveInsert(list, item);
+	}
 
+	private static <T extends Comparable <? super T>> void insertionSortRecursiveInsert(List <T> list, T item) {
+		// base case: list is empty or item is greater than or equal to the last element in the list
+		if (list.size() == 0 || item.compareTo(list.get(list.size() - 1)) >= 0){
+			list.add(item);
+			return;
+		}
+	
+		T last = list.remove(list.size() - 1);
+		insertionSortRecursiveInsert(list, item);
+		list.add(last);
+	}
 
 	public static <T extends Comparable <? super T>> void insertionSortIterative(List <T> list){
 		int i = 1;
--- W23 EECS2030_Lab4/src/eecs2030/lab4/sortExperiment.java
+++ W23 EECS2030_Lab4/src/eecs2030/lab4/sortExperiment.java
@@ -1,5 +1,8 @@
 package eecs2030.lab4;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -7,17 +10,55 @@ import java.util.Random;
 public class sortExperiment {
 	private static Random rng = new Random();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		List <Integer> list;
 		long start, end;
 
 		//You might need to change the 1M value to something smaller for other sorts
-		for (int i = 8; i <= 1_000_000; i *= 10){ 
+		
+		System.setOut(new PrintStream(new FileOutputStream("testrun.txt")));
+		System.out.println("DEFAULT");
+		for (int i = 8; i <= 10000; i *= 10){ 
 			list = createRandomList(i); 
+			
 			start = System.nanoTime();
 			Lists.defaultSort(list);
 			end = System.nanoTime();
-			System.out.println("N=" + i + "\nTime spent: " + (end - start)/1e6 + " ms");
+			
+			System.out.println("N=" + i + " Time spent: " + (end - start)/1e6 + " ms");
+		}
+		
+		System.out.println("\nRECURSIVE");
+		for (int i = 8; i <= 10000; i *= 10){ 
+			list = createRandomList(i); 
+			
+			start = System.nanoTime();
+			Lists.insertionSortRecursive(list);
+			end = System.nanoTime();
+			
+			System.out.println("N=" + i + " Time spent: " + (end - start)/1e6 + " ms");
+		}
+
+		System.out.println("\nITERATIVE");
+		for (int i = 8; i <= 10000; i *= 10){ 
+			list = createRandomList(i); 
+			
+			start = System.nanoTime();
+			Lists.insertionSortIterative(list);
+			end = System.nanoTime();
+		
+			System.out.println("N=" + i + " Time spent: " + (end - start)/1e6 + " ms");
+		}
+		
+		System.out.println("\nRECURSIVE STACKOVERFLOW");
+		for (int i = 8; i <= 100000; i *= 10){ 
+			list = createRandomList(i); 
+			
+			start = System.nanoTime();
+			Lists.insertionSortRecursive(list);
+			end = System.nanoTime();
+			
+			System.out.println("N=" + i + " Time spent: " + (end - start)/1e6 + " ms");
 		}
 	}
 
